fda56dd6e50e3f0048567549f0ffb078127fe7c0
rsmarincu <rsmarincu@gmail.com>
2019-07-10 20:55:52 +0100

    Initial Commit

---
+++ app.py
@@ -0,0 +1,167 @@
+from flask import Flask, render_template, request, jsonify
+import math
+import requests
+import numpy as np
+import pandas
+import json
+import utm
+from geopy.geocoders import Nominatim
+import configparser
+
+configs = configparser.RawConfigParser()
+configs.read('./api.ini')
+
+apiKey = configs['DEFAULT']['API_KEY']
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+geolocator = Nominatim(user_agent='rsmarincu')
+
+# //Routes...................................
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+@app.route('/hello',methods=['GET','POST'])
+def hello():
+    if request.method == 'POST':
+        print('Incoming..')
+        print(request.get_json())
+        json = request.get_json()
+        location = json['location']
+        coordinates = geolocator.geocode(location,timeout=10)
+        print (coordinates)
+        latitude = coordinates.latitude
+        longitude = coordinates.longitude
+        sendData = createData(size, latitude,longitude, distance)
+        return jsonify(sendData)
+    else:
+        return 'nothign'
+
+
+
+
+# //variables..................................................
+
+earthR=6371000
+west=math.radians(270)
+north=math.radians(0)
+
+latitude = 45.832119
+longitude = 6.865575
+size = 100
+distance = 50
+increase = 350
+
+# //Definitions.................................
+
+def getPointWest(initialLat,initialLong,distance):
+    initialLat = math.radians(initialLat)
+    initialLong = math.radians(initialLong)
+    newLat = math.asin(math.sin(initialLat) * math.cos(distance/earthR) + math.cos(initialLat) * math.sin(distance/earthR) * math.cos(west))
+    newLong = initialLong + math.atan2(math.sin(west) * math.sin(distance/earthR) * math.cos(initialLat) , math.cos(distance/earthR) - math.sin(initialLat)* math.sin(newLat))
+    newLat = math.degrees(newLat)
+    newLong = math.degrees(newLong)
+    location=(newLat,newLong)
+    return location
+
+
+def getPointNorth(initialLat,initialLong,distance):
+    initialLat = math.radians(initialLat)
+    initialLong = math.radians(initialLong)
+    newLat = math.asin(math.sin(initialLat) * math.cos(distance/earthR) + math.cos(initialLat) * math.sin(distance/earthR) * math.cos(north))
+    newLong = initialLong + math.atan2(math.sin(north) * math.sin(distance/earthR) * math.cos(initialLat) , math.cos(distance/earthR) - math.sin(initialLat)* math.sin(newLat))
+    newLat = math.degrees(newLat)
+    newLong = math.degrees(newLong)
+    location=(newLat,newLong)
+    return location
+
+
+def createGrid(size,lat,long,dist):
+    tuple_=()
+    points=np.empty(shape=(size,size),dtype=tuple)
+    northDist=0
+    westDist=0
+    for i in range(0,size):
+        points[i,0]=getPointNorth(lat,long,northDist)
+        nLat=points[i,0][0]
+        nLong=points[i,0][1]
+        for j in range(0,size):
+            points[i,j]=getPointWest(nLat,nLong,westDist)
+            westDist+=dist
+        westDist=0
+        northDist+=dist
+    return points
+
+
+def getElevations(points):
+    points = points.flatten()
+    locations = ''
+    elevations = []
+    counter = increase
+
+    _url = 'https://maps.googleapis.com/maps/api/elevation/json?locations={}&key={}'
+
+    for i,point in enumerate (points):
+
+        newS = '{},{}'.format(point[0],point[1])
+        locations = locations + newS + '|'
+
+        if i == counter:
+            locations = locations[:-1]
+            print(len(locations))
+            r = requests.get(_url.format(locations,apiKey))
+            print(r.status_code)
+
+            results= json.loads(r.content)
+
+            for result in results['results']:
+                elevations.append(result['elevation'])
+            locations = ''
+            counter += increase
+        elif i == len(points)-1:
+            locations = locations[:-1]
+            r = requests.get(_url.format(locations,apiKey))
+            results= json.loads(r.content)
+            print(r.content)
+            for result in results['results']:
+                elevations.append(result['elevation'])
+
+    return elevations
+
+def createXYgrid(size,dist):
+    grid = np.empty(shape=(size,size),dtype=tuple)
+    x = 0
+    y = 0
+    for i in range(0,size):
+        grid [i,0] = (x,y)
+        for j in range(0,size):
+            grid[i,j] = (x,y)
+            y+=dist
+        y = 0
+        x+=dist
+    return grid
+
+
+def createData(size,latitude,longitude,distance):
+
+    coordinates=createGrid(size,latitude,longitude,distance)
+    grid = createXYgrid(size,distance)
+    grid = grid.flatten().tolist()
+    elevations = getElevations(coordinates)
+    lleList = []
+    minElevation = 650000
+    for i,elev in enumerate (elevations):
+        if elev <  minElevation:
+            minElevation = elev
+        dict = {'lat':grid[i][0],
+                'long':grid[i][1],
+                'elev':elev}
+        lleList.append(dict)
+
+    lleJson = {'coordinate':lleList,
+                'lowest':minElevation,
+                'size': size
+                }
+    return lleJson
---
+++ static/script.js
@@ -0,0 +1,119 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100000);
+
+
+canvas = document.getElementById('mainCanvas')
+context = canvas.getContext('webgl')
+
+var light = new THREE.DirectionalLight(0xffffff, 1.1);
+light.castShadow = true;
+light.position.set(0.5, 1000, 0.1);
+
+
+var size = 100;
+var divisions = 10;
+var gridHelper = new THREE.GridHelper(size, divisions);
+gridHelper.colorGrid = '#000000';
+
+
+var renderer = new THREE.WebGLRenderer({
+  alpha: true,
+  canvas: canvas,
+  context: context
+});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+renderer.gammaOutput = true;
+renderer.gammaFactor = 2;
+
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+
+scene.add(light);
+
+
+document.getElementById('location').addEventListener('submit', function(e) {
+  e.preventDefault();
+  let location = document.getElementById('textBox').value;
+  fetch('/hello', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        location: location
+      })
+    })
+    .then((res) => res.json())
+    .then(function(json) {
+      for (let i = scene.children.length - 1; i >= 0; i--) {
+        if (scene.children[i].type === "Mesh")
+          scene.remove(scene.children[i]);
+      }
+      console.log(json.coordinate);
+      var vertices = [];
+      var holes = [];
+      json.coordinate.forEach(function(coord) {
+        var vert = new THREE.Vector3(coord.lat, coord.elev - json.lowest, coord.long);
+        vertices.push(vert);
+      });
+
+      var size = json.size
+      var length = size * size
+      var triangles, mesh;
+      var geometry = new THREE.Geometry();
+      var material = new THREE.MeshPhongMaterial({
+        color: 738827,
+        specular: 0x009900,
+        shininess: 2,
+        flatShading: true
+      });
+
+
+      geometry.vertices = vertices;
+
+      for (var i = 0; i < length - size; i++) {
+        if ((i + 1) % size == 0) {
+          continue;
+        }
+        geometry.faces.push(new THREE.Face3(i, i + 1, i + size));
+      }
+      for (var i = length; i > size; i--) {
+        if (i % size == 0) {
+          continue;
+        }
+        geometry.faces.push(new THREE.Face3(i, i - 1, i - size));
+      }
+
+      mesh = new THREE.Mesh(geometry, material);
+      mesh.material.side = THREE.DoubleSide;
+      scene.add(mesh);
+      camera.lookAt(mesh);
+
+
+    });
+});
+
+
+
+function createSphere(lat, long, elev) {
+  var geo = new THREE.SphereGeometry(10, 8, 8);
+  var sphere = new THREE.Mesh(geo, materialSphere);
+  sphere.position.x = lat;
+  sphere.position.y = long;
+  sphere.position.z = elev;
+  scene.add(sphere);
+  console.log('sphere added');
+};
+
+scene.add(gridHelper);
+camera.position.set(500, 500, 500);
+var animate = function() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+};
+
+animate();
